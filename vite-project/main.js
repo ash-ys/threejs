cbc1b28978fc51d28097c0b42c9a0671f6384e6f
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-05-26 14:14:47 +0545

    lighting

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,37 @@
+import './style.css'
 import * as THREE from 'three';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347, wireframe: true});
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(50,50,50)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+
+scene.add(pointLight, ambientLight)
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.z += 0.01;
+  torus.rotation.y += 0.005;
+  renderer.render(scene, camera);
+}
+
+animate()
+
